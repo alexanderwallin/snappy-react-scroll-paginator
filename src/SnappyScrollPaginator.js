6aa93b7282401988129861b63ea9aeaf6a639c35
Alexander Wallin <office@alexanderwallin.com>
2018-02-07 08:56:16 +0100

    Implements and tests initial scroll position

--- src/SnappyScrollPaginator.js
+++ src/SnappyScrollPaginator.js
@@ -1,18 +1,52 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { autobind } from 'core-decorators'
+
+const Axis = {
+  X: 'X',
+  Y: 'Y',
+}
 
 /**
  * Snappy Scroll Paginator
  */
 class SnappyScrollPaginator extends PureComponent {
+  static Axis = Axis
+
   static propTypes = {
-    page: PropTypes.number.isRequired,
+    axis: PropTypes.oneOf([Axis.X, Axis.Y]),
+    numPages: PropTypes.number.isRequired,
+    page: PropTypes.number,
+    pageHeight: PropTypes.number,
+    pageWidth: PropTypes.number,
+  }
+
+  static defaultProps = {
+    axis: Axis.Y,
+    page: 0,
+    pageHeight: 0,
+    pageWidth: 0,
+  }
+
+  $el = null
+
+  @autobind
+  handleRef($el) {
+    const { axis, page, pageHeight, pageWidth } = this.props
+
+    this.$el = $el
+
+    if (axis === Axis.X) {
+      this.$el.scrollLeft = page * pageWidth
+    } else if (axis === Axis.Y) {
+      this.$el.scrollTop = page * pageHeight
+    }
   }
 
   render() {
     const { page } = this.props
 
-    return <div className="SnappyScrollPaginator">{page}</div>
+    return <div ref={this.handleRef}>{page}</div>
   }
 }
 
--- tests/SnappyScrollPaginator.test.js
+++ tests/SnappyScrollPaginator.test.js
@@ -1,11 +1,32 @@
 import test from 'ava'
-import Enzyme from 'enzyme'
+import React from 'react'
+import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 import SnappyScrollPaginator from '../src/SnappyScrollPaginator.js'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-test('<SnappyScrollPaginator />', t => {
-  t.fail()
+test('has correct initial scroll position', t => {
+  const paginator1 = mount(
+    <SnappyScrollPaginator
+      axis={SnappyScrollPaginator.Axis.Y}
+      page={1}
+      numPages={3}
+      pageHeight={100}
+    />
+  )
+  t.is(paginator1.instance().$el.scrollTop, 100)
+
+  const paginator2 = mount(
+    <SnappyScrollPaginator
+      axis={SnappyScrollPaginator.Axis.X}
+      page={2}
+      numPages={3}
+      pageWidth={200}
+    />
+  )
+  t.is(paginator2.instance().$el.scrollLeft, 400)
 })
+
+
